61668698b9e8161c9c0a755303ec0d4c5d689ce4
makattack <makaylaroserussell@gmail.com>
2023-12-05 14:05:45 -0800

    fixing index

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function generatePassword() {
   var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
   var possibleCharacters = [];
+  var generatePassword = ['Password'];
 
   // get input and validate
   numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters.");
@@ -83,7 +84,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+ var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
